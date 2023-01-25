9ffd6ae1a3af794f12f8beee10790e41b93ea234
Giridharan <giridharanrock@gmail.com>
2023-01-25 13:05:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv')
+var HashMap = require('hashmap');
+dotenv.config()
+const app = express()
+var md5 = require('md5-slim');
+var map = new HashMap();
+
+const sourceSysChannel = 'ONL'
+const secretCode = '123456789012'
+
+
+app.get('/msgdigest', (req,res)=>{
+    const msgId = req.query['msgId']
+    const SECURE_SECRET =  'O4UGgzQ5GkMefyb01oQ1299565704666'
+    const msgConcat = msgId.concat(sourceSysChannel.concat(secretCode));
+    res.json({
+        "MsgId" : msgId,
+        "MsgDigest": md5(msgConcat.concat(SECURE_SECRET)).toUpperCase()
+    })
+})
+
+app.get('/', (req,res) => {
+    res.send({"message":"alive"})
+    })
+    
+app.listen(process.env.PORT,()=>{console.log("server up and running in "+ process.env.PORT)})
